40ffdadfc7b06308485ee316c399055b2259acec
Bobako <abqu144@gmail.com>
2022-07-16 13:19:12 +0300

    autosend in args

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from sms_handler import app, sms_api_worker
+"""App entry point. If 'autosend' keyword in sys.argv app will
+automatically resend all unsuccessful messages from primary service """
+import sys
 import threading
 
-if __name__ == '__main__':
-    #t1 = threading.Thread(target=sms_api.run).start()
-    t2 = threading.Thread(target=app.run).start()
+from sms_handler import app, sms_api_worker
 
+if __name__ == '__main__':
+    if "autosend" in sys.argv:
+        sms_api_worker_thread = threading.Thread(target=sms_api_worker.run).start()
+    app_thread = threading.Thread(target=app.run).start()
