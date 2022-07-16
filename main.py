d1723d460382eccb1babbfa2a9035f718f9fb3f9
Bobako <abqu144@gmail.com>
2022-07-16 13:29:36 +0300

    port in ini

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ automatically resend all unsuccessful messages from primary service """
 import sys
 import threading
 
-from sms_handler import app, sms_api_worker
+from sms_handler import app, sms_api_worker, config
 
 if __name__ == '__main__':
     if "autosend" in sys.argv:
         sms_api_worker_thread = threading.Thread(target=sms_api_worker.run).start()
-    app_thread = threading.Thread(target=app.run).start()
+    app_thread = threading.Thread(target=app.run, kwargs={"port": int(config["SITE"]["port"])}).start()
