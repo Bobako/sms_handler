57473fb7ddfd77bb4405dbcea30462b60ca5677d
bobako <abqu144@gmail.com>
2022-08-15 09:05:35 +0300

    Demo created

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import threading
 from sms_handler import app, sms_api_worker, config
 
 if __name__ == '__main__':
-    sms_api_worker_thread = threading.Thread(target=sms_api_worker.run,
-                                             kwargs={"auto_send": "auto_send" in sys.argv}).start()
+    # sms_api_worker_thread = threading.Thread(target=sms_api_worker.run,
+    #                                         kwargs={"auto_send": "auto_send" in sys.argv}).start()
+    # disabled in demo
     app_thread = threading.Thread(target=app.run, kwargs={"port": int(config["SITE"]["port"])}).start()
